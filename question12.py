e072447b48c39f5b4b589354e685e27bf1ae8712
SCurtis95 <curt0061@algonquinlive.com>
2019-09-17 13:44:05 -0400

    q12

--- question12.py
+++ question12.py
@@ -5,8 +5,7 @@ temp = input("Please enter in the degrees in fahrenheit: ")
 fahTemp = int(temp)
 
 celsius = ((fahTemp - 32) / 1.8)
-
-cel = int(celsius)
+cel = round(celsius)
 finalTemp = str(cel)
 
 print("The temprature " + temp + " in fahrenheit is " + finalTemp + " degrees in celsius")
